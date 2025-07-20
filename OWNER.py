287f0a17b685c749e6a996f474f510942fcfba41
djjjdhbsjsdb <tfyl52601@gmail.com>
2025-07-20 11:38:32 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["G_7_Rr"]
+OWNER = ["ssuusu"]
 OWNER_NAME = "teto"
-BOT_TOKEN = "6432841340:AAFmzvjpBRc025leCsXzqafV7N1TLT_VLCc"
+BOT_TOKEN = "8197599028:AAGvyiVC2cWbYcRxSH7-lvxQiQFNa8hgpaI"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
-CHANNEL = "https://t.me/CH_NR"
-GROUP = "https://t.me/t7_AU"
+CHANNEL = "https://t.me/n3nn333"
+GROUP = "https://t.me/ozzcz"
 VIDEO = "https://graph.org/file/061951daec85b0f94b83c.jpg"
 LOGS = "zombie2"
 
